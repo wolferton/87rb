4cb807e77dc6f8d4ec1cd0c02242fc37f0188ee4
Ben Halstead <ben.halstead@brightpearl.com>
2016-03-31 13:47:56 +0100

    Fixed DB ownership. Schema development.

--- schema/87rb-core.sql
+++ schema/87rb-core.sql
@@ -1,6 +1,39 @@
 CREATE SCHEMA admin;
+CREATE SCHEMA jobs;
+CREATE SCHEMA auth;
 
+--Admin schema: housekeeping
 CREATE TABLE admin.schema_version (
-	version char(5),
-	applied_on date
+	version CHAR(5),
+	applied_on TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+
+
+--Authorisation schema: users, roles and permissions
+CREATE SEQUENCE auth.actor_ids_seq START 1000;
+
+CREATE TABLE auth.actor (
+    id INTEGER DEFAULT nextval('auth.actor_ids_seq') PRIMARY KEY,
+    name VARCHAR(64)
+);
+
+ALTER SEQUENCE auth.actor_ids_seq OWNED BY auth.actor.id;
+
+INSERT INTO
+    auth.actor (id, name)
+VALUES
+    (50, 'System'),
+    (60, 'Anonymous UI User'),
+    (70, 'Anonymous API User');
+
+
+
+--Jobs schema: operational/transactional data
+CREATE TABLE jobs.job (
+    id SERIAL PRIMARY KEY,
+    created_by INTEGER REFERENCES auth.actor(id) NOT NULL,
+    created_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_by INTEGER REFERENCES auth.actor(id) NOT NULL,
+    updated_on TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
--- templates/init-core-db.sh
+++ templates/init-core-db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 psql -c "CREATE ROLE {{DbAdminUser}} LOGIN PASSWORD '{{DbAdminPassword}}' CREATEDB"
 psql -c "CREATE ROLE {{DbApiUser}} LOGIN PASSWORD '{{DbApiPassword}}'"
-psql -c "CREATE DATABASE {{DatabaseName}}"
+psql -c "CREATE DATABASE {{DatabaseName}} OWNER={{DbAdminUser}}  ENCODING='UTF8'"
+psql -c "ALTER DATABASE {{DatabaseName}} SET timezone TO 'UTC'"
 PGPASSWORD={{DbAdminPassword}} psql -U{{DbAdminUser}} -f{{SchemaFile}} {{DatabaseName}}
